065b688befef6261b72bb0ba1efcb2d9b768fd18
jiezhuzzz <jiezzz@duck.com>
2025-06-22 21:16:20 -0500

    update

--- apps/zellij.nix
+++ apps/zellij.nix
@@ -1,10 +1,10 @@
 {
   programs.zellij = {
     enable = true;
-    enableFishIntegration = true;
-    enableZshIntegration = true;
-    attachExistingSession = true;
-    exitShellOnExit = true;
+    # enableFishIntegration = true;
+    # enableZshIntegration = true;
+    # attachExistingSession = true;
+    # exitShellOnExit = true;
     settings = {
       theme = "nord";
     };
