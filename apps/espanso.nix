d839ca440a66bc8e608747e51e50117a8f77e9cf
jiezhuzzz <jiezzz@duck.com>
2025-07-14 14:46:43 -0500

    Add espanso and jujutsu configurations to home-manager, and update zellij keybindings

---
+++ apps/espanso.nix
@@ -0,0 +1,37 @@
+{
+  services.espanso = {
+    enable = true;
+    matches = {
+      base = {
+        matches = [
+          {
+            trigger = "kx";
+            replace = "Kexin";
+          }
+        ];
+      };
+      latex = {
+        matches = [
+          {
+            trigger = ";m";
+            replace = "\\($|$\\)";
+          }
+        ];
+      };
+      global_vars = {
+        global_vars = [
+          {
+            name = "currentdate";
+            type = "date";
+            params = {format = "%d/%m/%Y";};
+          }
+          {
+            name = "currenttime";
+            type = "date";
+            params = {format = "%R";};
+          }
+        ];
+      };
+    };
+  };
+}
