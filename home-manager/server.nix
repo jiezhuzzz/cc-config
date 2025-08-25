6ccc91dc3ba38a38cb517f94eeb7040147d8bf6a
jiezhuzzz <jiezzz@duck.com>
2025-08-24 19:47:34 -0500

    Add shpool package to home-manager server configuration

--- home-manager/server.nix
+++ home-manager/server.nix
@@ -11,6 +11,7 @@
   # Server specific packages
   home.packages = with pkgs; [
     codeql
+    shpool
     (writeShellScriptBin "aflpp" (builtins.readFile ../scripts/aflpp.sh))
     (writeShellScriptBin "mx" (builtins.readFile ../scripts/multiplier.sh))
   ];
