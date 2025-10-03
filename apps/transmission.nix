ddf5fcd00c272e70ba0573eacf3e5074309d258b
jiezhuzzz <jiezzz@duck.com>
2025-10-03 01:43:13 -0500

    transmission whitelist

--- apps/transmission.nix
+++ apps/transmission.nix
@@ -5,6 +5,7 @@
     openRPCPort = true;
     settings = {
       rpc-bind-address = "0.0.0.0";
+      rpc-whitelist = "127.0.0.1,192.168.1.*";
       download-dir = "/storage/transmission/downloads";
       incomplete-dir = "/storage/transmission/incomplete";
       watch-dir = "/storage/transmission/watch";
