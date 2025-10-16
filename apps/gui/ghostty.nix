86c3544f55029cbf930e4582870c2c71bc41e68b
jiezhuzzz <jiezzz@duck.com>
2025-10-16 15:27:16 -0500

    Update OnePassword and SSH configurations, add Zed editor settings, and modify Ghostty integration in Nix files

--- apps/gui/ghostty.nix
+++ apps/gui/ghostty.nix
@@ -1,7 +1,15 @@
-{
+{pkgs, ...}: {
   programs.ghostty = {
     enable = true;
+    package = pkgs.ghostty-bin;
     enableZshIntegration = true;
     enableFishIntegration = true;
+    installVimSyntax = true;
+    settings = {
+      window-padding-x = 10;
+      font-size = 16;
+      clipboard-trim-trailing-spaces = true;
+      mouse-hide-while-typing = true;
+    };
   };
 }
