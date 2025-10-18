dc031d479b23a8356b92a85bf09cd03269d3d2c5
jiezhuzzz <jiezzz@duck.com>
2025-10-18 17:18:31 -0500

    config fzf with zsh-helix-mode

--- apps/zsh.nix
+++ apps/zsh.nix
@@ -11,5 +11,16 @@
       "Aloxaf/fzf-tab"
       "lipov3cz3k/zsh-uv"
     ];
+    initContent = ''
+      # fzf
+      zhm_wrap_widget fzf-completion zhm_fzf_completion
+      bindkey '^I' zhm_fzf_completion
+      # fzf history
+      zhm_wrap_widget fzf-history-widget zhm_fzf_history
+      bindkey '^R' zhm_fzf_history
+      # fzf-tab
+      zhm_wrap_widget fzf-tab-complete zhm_fzf_tab_complete
+      bindkey '^I' zhm_fzf_tab_complete
+    '';
   };
 }
