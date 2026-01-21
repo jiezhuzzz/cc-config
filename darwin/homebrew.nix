439a90f401a095238292796b03ab2b767624398d
jiezhuzzz <jiezzz@duck.com>
2026-01-21 00:17:56 -0600

    chore(homebrew): update application list

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -6,7 +6,6 @@
       "raycast"
       "balenaetcher"
       "cursor"
-      "hazeover"
       "istat-menus"
       "kaleidoscope"
       "keka"
@@ -20,23 +19,14 @@
       "homerow"
       "1password"
       "karabiner-elements"
-      # "bartender"
-      # "ghostty"
-      "pronotes"
-      # "visual-studio-code"
-      "pika"
       "pdf-expert"
       "adguard"
       "hazel"
-      "openinterminal"
       "nx-studio"
-      "notion"
-      "notion-calendar"
-      "notion-mail"
-      "spotify"
       "cleanshot"
       "figma"
       "plex"
+      "wakatime"
     ];
     masApps = {
       "WeChat" = 836500024;
@@ -45,7 +35,6 @@
       "Pixelmator Pro" = 1289583905;
       "Infuse" = 1136220934;
       "Amphetamine" = 937984704;
-      # "Endel" = 1346247457;
       # "Bear" = 1091189122;
       "Obsidian Web Clipper" = 6720708363;
       # "AdGuard for Safari" = 1440147259;
@@ -55,11 +44,7 @@
       "SnippetsLab" = 1006087419;
       "Noir" = 1592917505;
       "Immersive Translate" = 6447957425;
-      # "Affinity Designer 2" = 1616831348;
-      # "Affinity Photo 2" = 1616822987;
-      # "Affinity Publisher 2" = 1606941598;
       "CloudMounter" = 1130254674;
-      # "TypeIt4Me 7" = 6474688391;
     };
     onActivation = {
       autoUpdate = true;
