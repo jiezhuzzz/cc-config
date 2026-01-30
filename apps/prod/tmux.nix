316f41f7ac86652be20fe96866f5bd3b86ed5c0c
jiezhuzzz <jiezzz@duck.com>
2026-01-30 11:48:52 +0800

    feat(tmux): enhance status bar with catppuccin theme

--- apps/prod/tmux.nix
+++ apps/prod/tmux.nix
@@ -1,5 +1,4 @@
 {
-  config,
   pkgs,
   ...
 }: {
@@ -10,28 +9,39 @@
     baseIndex = 1;
     disableConfirmationPrompt = true;
     mouse = true;
-    terminal = "screen-256color";
+    terminal = "tmux-256color";
     plugins = with pkgs.tmuxPlugins; [
       tmux-which-key
+      cpu
+      tmux-fzf
       # nord
     ];
     extraConfig = ''
       # status bar
-      set -g status-style bg=default,fg=black,bright
+      set -g status-right-length 100
+      set -g status-left-length 100
       set -g status-left ""
-      set -g status-right "#[fg=black,bright]#S"
+      set -g status-right "#{E:@catppuccin_status_application}"
+      set -agF status-right "#{E:@catppuccin_status_cpu}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_uptime}"
 
       # windows button
-      set -g window-status-format "●"
-      set -g window-status-current-format "●"
-      set -g window-status-current-style "#{?window_zoomed_flag,fg=yellow,fg=magenta,nobold}"
-      set -g window-status-bell-style "fg=red,nobold"
+      #set -g window-status-format "●"
+      #set -g window-status-current-format "●"
+      #set -g window-status-current-style "#{?window_zoomed_flag,fg=yellow,fg=magenta,nobold}"
+      #set -g window-status-bell-style "fg=red,nobold"
 
       # options
       set -g renumber-windows on
 
       # pane borders
-      set -g pane-border-lines simple
+      # set -g pane-border-lines simple
     '';
   };
+
+  catppuccin.tmux.extraConfig = ''
+    set -g @catppuccin_window_status_style "rounded"
+    set -g @catppuccin_status_modules_right "application cpu session uptime"
+  '';
 }
