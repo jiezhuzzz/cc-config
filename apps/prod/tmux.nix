71a2b4a50cee29244b9615afa626f7c1d72d662f
jiezhuzzz <jiezzz@duck.com>
2026-02-01 21:53:09 +0800

    refactor(tmux): clean up cpu plugin and formatting

--- apps/prod/tmux.nix
+++ apps/prod/tmux.nix
@@ -1,7 +1,4 @@
-{
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     prefix = "`";
@@ -12,9 +9,7 @@
     terminal = "tmux-256color";
     plugins = with pkgs.tmuxPlugins; [
       tmux-which-key
-      cpu
       tmux-fzf
-      # nord
     ];
     extraConfig = ''
       # status bar
@@ -22,26 +17,19 @@
       set -g status-left-length 100
       set -g status-left ""
       set -g status-right "#{E:@catppuccin_status_application}"
+
       set -agF status-right "#{E:@catppuccin_status_cpu}"
+      run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
+
       set -ag status-right "#{E:@catppuccin_status_session}"
       set -ag status-right "#{E:@catppuccin_status_uptime}"
 
-      # windows button
-      #set -g window-status-format "●"
-      #set -g window-status-current-format "●"
-      #set -g window-status-current-style "#{?window_zoomed_flag,fg=yellow,fg=magenta,nobold}"
-      #set -g window-status-bell-style "fg=red,nobold"
-
       # options
       set -g renumber-windows on
-
-      # pane borders
-      # set -g pane-border-lines simple
     '';
   };
 
   catppuccin.tmux.extraConfig = ''
     set -g @catppuccin_window_status_style "rounded"
-    set -g @catppuccin_status_modules_right "application cpu session uptime"
   '';
 }
