65765d0732a4b060faee355317b7ca9e27c8f2fe
jiezhuzzz <jiezzz@duck.com>
2026-02-18 05:24:25 +0000

    refactor(op): simplify path handling in 1Password secrets

--- apps/prod/op.nix
+++ apps/prod/op.nix
@@ -3,17 +3,15 @@
   lib,
   pkgs,
   ...
-}: let
-  homeDir = config.home.homeDirectory;
-in {
+}: {
   programs.onepassword-secrets = {
     enable = true;
-    tokenFile = "${homeDir}/.op-token";
+    tokenFile = "${config.home.homeDirectory}/.op-token";
     secrets =
       {
         signingPrivateKey = {
           reference = "op://SSH/Signing/private_key?ssh-format=openssh";
-          path = "${homeDir}/.ssh/signing.private";
+          path = ".ssh/signing.private";
           group =
             if pkgs.stdenv.isLinux
             then config.home.username
@@ -21,7 +19,7 @@ in {
         };
         githubPrivateKey = {
           reference = "op://SSH/GitHub/private_key?ssh-format=openssh";
-          path = "${homeDir}/.ssh/github.private";
+          path = ".ssh/github.private";
           group =
             if pkgs.stdenv.isLinux
             then config.home.username
@@ -31,15 +29,15 @@ in {
       // lib.optionalAttrs pkgs.stdenv.isDarwin {
         sshPrivateKey = {
           reference = "op://SSH/Personal/private_key?ssh-format=openssh";
-          path = "${homeDir}/.ssh/personal.private";
+          path = ".ssh/personal.private";
         };
         labPrivateKey = {
           reference = "op://SSH/Lab/private_key?ssh-format=openssh";
-          path = "${homeDir}/.ssh/lab.private";
+          path = ".ssh/lab.private";
         };
         ccPrivateKey = {
           reference = "op://SSH/Chameleon/private_key?ssh-format=openssh";
-          path = "${homeDir}/.ssh/cc.private";
+          path = ".ssh/cc.private";
         };
       };
   };
