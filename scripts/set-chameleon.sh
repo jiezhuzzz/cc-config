6a975bb3af0f6d42d1a012e7d9aeb064658eb0d3
jiezhuzzz <jiezzz@duck.com>
2026-02-19 02:44:37 +0800

    feat(scripts): add uidmap install and trusted-users nix config to set-chameleon

--- scripts/set-chameleon.sh
+++ scripts/set-chameleon.sh
@@ -10,11 +10,12 @@ for ip in "${argc_ips[@]}"; do
     scp ~/.envs cc@"$ip":~/.envs
     scp ~/.op-token cc@"$ip":~/.op-token
     # update and upgrade packages
-    ssh cc@"$ip" -- "sudo apt update && sudo apt upgrade -y"
+    ssh cc@"$ip" -- "sudo apt update && sudo apt upgrade -y && sudo apt install -y uidmap"
     # install Nix
     ssh cc@"$ip" -- "curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm"
     # install configuration
     ssh cc@"$ip" -- "nix run nixpkgs#home-manager -- switch --flake github:jiezhuzzz/cc-config#cc"
+    ssh cc@"$ip" -- "sudo echo 'trusted-users = root cc' >> /etc/nix/nix.custom.conf"
     # set up the terminfo
     infocmp -x xterm-ghostty | ssh cc@"$ip" -- tic -x -
     echo "Finished setting up $ip"
