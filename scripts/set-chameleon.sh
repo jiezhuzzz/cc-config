0bd74e2d159877890af91e9425f7cf08f62f5374
jiezhuzzz <jiezzz@duck.com>
2026-02-18 12:51:29 +0800

    feat(scripts): add set-chameleon.sh for remote server provisioning

---
+++ scripts/set-chameleon.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# @arg ips+       Positional param
+
+eval "$(argc --argc-eval "$0" "$@")"
+
+for ip in "${argc_ips[@]}"; do
+    # copy 1password token and envs
+    scp ~/.envs cc@"$ip":~/.envs
+    scp ~/.op-token cc@"$ip":~/.op-token
+    # update and upgrade packages
+    ssh cc@"$ip" -- "sudo apt update && sudo apt upgrade -y"
+    # install Nix
+    ssh cc@"$ip" -- "curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm"
+    # install configuration
+    ssh cc@"$ip" -- "nix run nixpkgs#home-manager -- switch --flake github:jiezhuzzz/cc-config#cc"
+    # set up the terminfo
+    infocmp -x xterm-ghostty | ssh cc@"$ip" -- tic -x -
+    echo "Finished setting up $ip"
+done
