9990b9d1a0dfb16ac1b0609c0eb07d067f5185d3
jiezhuzzz <jiezzz@duck.com>
2026-02-19 02:52:46 +0800

    feat(zsh): add atuin history widget binding for ^R

--- apps/prod/zsh.nix
+++ apps/prod/zsh.nix
@@ -23,17 +23,15 @@
       # fzf
       zhm_wrap_widget fzf-completion zhm_fzf_completion
       bindkey '^I' zhm_fzf_completion
+      # # fzf history
+      # zhm_wrap_widget fzf-history-widget zhm_fzf_history
+      # bindkey '^R' zhm_fzf_history
+      # atuin history
+      zhm_wrap_widget _atuin_search_widget zhm_atuin_search
+      bindkey '^R' zhm_atuin_search
       # fzf-tab
       zhm_wrap_widget fzf-tab-complete zhm_fzf_tab_complete
       bindkey '^I' zhm_fzf_tab_complete
-
-      # Wrap atuin widgets for helix mode compatibility
-      zhm_wrap_widget _atuin_search_widget zhm_atuin_search
-      bindkey '^R' zhm_atuin_search
-
-      # Wrap up arrow for atuin history navigation
-      zhm_wrap_widget _atuin_up_search_widget zhm_atuin_up_search
-      bindkey '^[[A' zhm_atuin_up_search  # Up arrow
     '';
   };
 }
